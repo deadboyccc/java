8a420a1b61840de9cea225ba750914bf824ef679
Ahmed Mohammed Ali <ahmed.mohammed.ali@gmail.com>
2025-03-28 19:06:13 +0300

    skimming over the intro parts of the course

--- src/intro/App.java
+++ src/intro/App.java
@@ -1,5 +1,8 @@
 package intro;
 
+import java.util.LinkedList;
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -13,5 +16,25 @@ public class App {
         System.out.println(Long.MAX_VALUE);
         Human test = new Human("Joe", Long.MAX_VALUE);
         System.out.println(test);
+        System.out.println(factorial(5));
+        LinkedList<Human> humansList = new LinkedList<>();
+        humansList.add(test);
+        humansList.add(new Human("doe", Byte.MAX_VALUE));
+        for (Human human : humansList) {
+            System.out.println(human);
+        }
+        HashMap<String, String> testMap = new HashMap<>();
+        testMap.put("a", "first");
+        testMap.put("b", "second");
+        System.out.println(testMap.get("a"));
+
+    }
+
+    public static long factorial(int n) {
+        if (n == 1 || n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+
     }
 }
