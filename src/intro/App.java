f46c1e7c8059296f35eecbd8894e7e197ac1cc3a
Ahmed Mohammed Ali <ahmed.mohammed.ali@gmail.com>
2025-03-28 21:53:40 +0300

    Impelemented BS

--- src/intro/App.java
+++ src/intro/App.java
@@ -28,6 +28,29 @@ public class App {
         testMap.put("b", "second");
         System.out.println(testMap.get("a"));
 
+        int[] binarySearchArr = { -5, -2, 0, 3, 7, 9, 11, 25 };
+        int found = binarySearch(binarySearchArr, 3);
+        System.out.println(found);
+
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 
     public static long factorial(int n) {
@@ -37,4 +60,5 @@ public class App {
         return n * factorial(n - 1);
 
     }
+
 }
