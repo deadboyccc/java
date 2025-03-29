16798d8737d3a8adc1f4582510abf4cec1b80d6f
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-03-29 19:02:55 +0300

    refactored the main entry point (app.java)

--- src/intro/App.java
+++ src/intro/App.java
@@ -11,6 +11,10 @@ import java.util.random.*;
 @SuppressWarnings("unused")
 public class App {
     public static void main(String[] args) throws Exception {
+
+    }
+
+    private static void OOPtest1() {
         Animal animal1 = new Animal("generic Animal", "huge", 400);
         doAnimalStuff(animal1, "slow");
         Dog dog = new Dog();
@@ -31,7 +35,6 @@ public class App {
         System.out.println("########");
         Fish fish = new Fish("Shark", 40, 2, 3);
         doAnimalStuff(fish, "fast");
-
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
