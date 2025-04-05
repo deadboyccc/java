d5bab674455211331e85310e6dd0e3eb24f05ec5
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-04-05 17:59:34 +0300

    List Interface (LinkedLists, ArrayList) and Iterators/ListIterators

--- src/intermediate/Collections.java
+++ src/intermediate/Collections.java
@@ -1,6 +1,12 @@
 package intermediate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Collections {
   public static void main(String[] args) {
@@ -11,6 +17,27 @@ public class Collections {
     // <> diamond operator :
     objectList.add(new GrosseryItem("Butter"));
     objectList.add(new GrosseryItem("Yogurt"));
+    ArrayList<Integer> intList = new ArrayList<>(List.of(10, 11, 187, 1, 2, 8));
+    intList.sort(Comparator.reverseOrder());
+    // :3
+    System.out.println(Arrays.toString(intList.toArray()));
+    var intLinkedList = new LinkedList<Integer>(List.of(1, 2, 3, 4, 5));
+    Iterator<Integer> it = intLinkedList.iterator(); // limited compared to ListIterator
+
+    while (it.hasNext()) {
+      System.out.println(it.next());
+
+    }
+    // probably not the cleaniest xd
+    System.out.println("-".repeat(20));
+    ListIterator<Integer> enhancedIt = intLinkedList.listIterator(intLinkedList.size());
+    while (enhancedIt.hasPrevious()) {
+      // System.out.println(enhancedIt.previous());
+      if (enhancedIt.previous().equals(4)) {
+        enhancedIt.remove();
+      }
+    }
+    System.out.println(intLinkedList);
 
   }
 
