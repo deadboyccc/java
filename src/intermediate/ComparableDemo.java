b861b507d08ca3dc9ec1177635597c74a29ff17a
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-04-20 10:03:44 +0300

    Comparator as a nested class to add extra sort functionality

--- src/intermediate/ComparableDemo.java
+++ src/intermediate/ComparableDemo.java
@@ -1,6 +1,8 @@
 package intermediate;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
 
 // comparable interface
 public class ComparableDemo {
@@ -27,6 +29,56 @@ public class ComparableDemo {
 
         System.out.println("A:" + (int) 'A' + " " + "a:" + (int) 'a');
         System.out.println("B:" + (int) 'B' + " " + "b:" + (int) 'b');
+        Student tim = new Student("Tim");
+        Student[] students = { new Student("Bob"), new Student("Alice"),
+                new Student("Tim"), new Student("Zoe") };
+        Arrays.sort(students);
+        System.out.println(Arrays.toString(students));
+        Arrays.sort(students, new StudentGPAComparator().reversed());
 
     }
+
+}
+
+class StudentGPAComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        int gpaComparison = Double.compare(o1.GPA, o2.GPA);
+        return gpaComparison != 0 ? gpaComparison : o1.name.compareTo(o2.name);
+    }
+
+}
+
+class Student implements Comparable<Student> {
+
+    private static int LAST_ID = 1000;
+    private static Random random = new Random();
+
+    String name;
+    private int studentId;
+    protected double GPA;
+
+    public Student(String name) {
+
+        this.name = name;
+        this.studentId = LAST_ID++;
+        this.GPA = 1.0 + (3.0 * random.nextDouble());
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", studentId=" + studentId + ", GPA=" + GPA + "]";
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        // non generic
+        // Student student = (Student) o;
+        return Integer.valueOf(this.studentId).compareTo(Integer.valueOf(o.studentId));
+        // return this.name.compareTo(o.name);
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+
 }
