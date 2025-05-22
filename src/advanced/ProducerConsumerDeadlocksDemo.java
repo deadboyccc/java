dfbbf5a818029661d67dd95a99246d7755b8da59
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2025-05-22 12:58:27 +0300

    10. deadlock on a shared MessageRepo

---
+++ src/advanced/ProducerConsumerDeadlocksDemo.java
@@ -0,0 +1,103 @@
+package advanced;
+
+import java.util.Random;
+
+//shared
+class MessageRepo {
+    private String message;
+    private boolean hasMessage = false;
+
+    public synchronized String read() {
+        while (!hasMessage) {
+
+        }
+        hasMessage = false;
+        return message;
+    }
+
+    public synchronized void write(String message) {
+        while (hasMessage) {
+
+        }
+        hasMessage = true;
+        this.message = message;
+
+    }
+
+}
+
+class MessageWriter implements Runnable {
+
+    private MessageRepo outgoingMessage;
+    private final String text = """
+            Joe Biden vs Humpty Dumpty 1
+            Joe Biden vs Humpty Dumpty 2
+            Joe Biden vs Humpty Dumpty 3
+                    """;
+
+    public MessageWriter(MessageRepo outgoingMessage) {
+        this.outgoingMessage = outgoingMessage;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String[] lines = text.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            outgoingMessage.write(lines[i]);
+            try {
+
+                Thread.sleep(random.nextInt(500, 2000));
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+
+        }
+        outgoingMessage.write("Finished");
+
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
+
+class MessageReader implements Runnable {
+    private MessageRepo incomingMessage;
+
+    public MessageReader(MessageRepo incomingMessage) {
+        this.incomingMessage = incomingMessage;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String latestMessage = "";
+        do {
+
+            try {
+                Thread.sleep(random.nextInt(500, 2000));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            latestMessage = incomingMessage.read();
+            System.out.println(latestMessage);
+        } while (!latestMessage.equals("Finished"));
+    }
+
+}
+
+public class ProducerConsumerDeadlocksDemo {
+    public static void main(String[] args) {
+
+        MessageRepo messageRepo = new MessageRepo();
+
+        Thread reader = new Thread(new MessageReader(messageRepo));
+        Thread writer = new Thread(new MessageWriter(messageRepo));
+
+        reader.start();
+        writer.start();
+    }
+
+}
