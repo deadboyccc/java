dc486e4845674ae65b2c89d7f14e0fe3cd538722
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2025-05-28 10:45:31 +0300

    Add RecursiveSumTask for parallel sum computation in ParallelProcessesDemo

--- src/advanced/ParallelProcessesDemo.java
+++ src/advanced/ParallelProcessesDemo.java
@@ -7,7 +7,50 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+class RecursiveSumTask extends RecursiveTask<Long> {
+    private final long[] numbers;
+    private final int start;
+    private final int end;
+    private final int division;
+
+    public RecursiveSumTask(long[] numbers, int start, int end, int division) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+        this.division = division;
+    }
+
+    @Override
+    protected Long compute() {
+        if ((end - start) <= (numbers.length / division)) {
+            System.out.println(start + " : " + end);
+
+            long sum = 0;
+
+            for (int i = start; i < end; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+
+        } else {
+
+            int mid = (start + end) / 2;
+
+            RecursiveSumTask leftTask = new RecursiveSumTask(numbers, start, mid, division);
+            RecursiveSumTask rightTask = new RecursiveSumTask(numbers, mid, end, division);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            return leftTask.join() + rightTask.join();
+        }
+    }
+
+}
 
 public class ParallelProcessesDemo {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
@@ -25,7 +68,7 @@ public class ParallelProcessesDemo {
 
         int cores = Runtime.getRuntime().availableProcessors();
         System.out.println("Number of cores: " + cores);
-        var threadPool = Executors.newWorkStealingPool(cores);
+        ForkJoinPool threadPool = ForkJoinPool.commonPool();
 
         List<Callable<Long>> tasks = new ArrayList<>();
 
@@ -47,6 +90,12 @@ public class ParallelProcessesDemo {
             });
         }
         List<Future<Long>> futures = threadPool.invokeAll(tasks);
+
+        System.out.println("Parallelism Level = " + threadPool.getParallelism());
+        System.out.println("Pool Size = " + threadPool.getPoolSize());
+        System.out.println("Steal Size = " + threadPool.getStealCount());
+        System.out.println("Free Mem = " + Runtime.getRuntime().freeMemory());
+
         long taskSum = 0;
         for (Future<Long> future : futures) {
             taskSum += future.get();
@@ -54,6 +103,9 @@ public class ParallelProcessesDemo {
         }
         System.out.println("parallel Sum= " + taskSum);
 
+        RecursiveTask task = new RecursiveSumTask(numbers, 0, numbersLength, 8);
+        long forkJoinSum = (long) threadPool.invoke(task);
+        System.out.println("forkJoinSum= " + forkJoinSum);
         threadPool.shutdown();
         System.out.println(threadPool.getClass().getName());
     }
