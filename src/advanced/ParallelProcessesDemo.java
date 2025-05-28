342f255aad93d49c2f38d3b025e1911fc153a582
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2025-05-28 10:26:52 +0300

    Refactor ParallelProcessesDemo for improved readability and structure

--- src/advanced/ParallelProcessesDemo.java
+++ src/advanced/ParallelProcessesDemo.java
@@ -11,19 +11,30 @@ import java.util.concurrent.Future;
 
 public class ParallelProcessesDemo {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+
         int numbersLength = 100_000;
-        long[] numbers = new Random().longs(numbersLength, 1, numbersLength).toArray();
+        long[] numbers = new Random().longs(numbersLength,
+                1,
+                numbersLength)
+                .toArray();
 
         long sum = Arrays.stream(numbers).sum();
         System.out.println("Sum= " + sum);
+
+        System.out.println("____".repeat(20));
 
         int cores = Runtime.getRuntime().availableProcessors();
         System.out.println("Number of cores: " + cores);
         var threadPool = Executors.newWorkStealingPool(cores);
 
         List<Callable<Long>> tasks = new ArrayList<>();
+
+        // number of tasks
         int taskNo = 10;
+
+        // total work size ( array length) / number of tasks = split per task
         int splitCount = numbersLength / taskNo;
+
         for (int i = 0; i < taskNo; i++) {
             int start = i * splitCount;
             int end = start + splitCount;
@@ -42,6 +53,7 @@ public class ParallelProcessesDemo {
 
         }
         System.out.println("parallel Sum= " + taskSum);
+
         threadPool.shutdown();
         System.out.println(threadPool.getClass().getName());
     }
