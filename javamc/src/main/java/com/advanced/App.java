ab14e71bac964d408d47d86c2afc1959ef562224
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-07-15 16:27:58 +0300

    init: new maven starter

---
+++ javamc/src/main/java/com/advanced/App.java
@@ -0,0 +1,13 @@
+package com.advanced;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
