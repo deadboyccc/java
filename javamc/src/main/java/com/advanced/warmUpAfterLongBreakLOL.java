a830a136b7ddd4ea0a380b8a5371719caa72444f
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-08-30 16:07:43 +0300

    Add SimpleServerChannel class for handling client connections and echo functionality

--- javamc/src/main/java/com/advanced/warmUpAfterLongBreakLOL.java
+++ javamc/src/main/java/com/advanced/warmUpAfterLongBreakLOL.java
@@ -5,12 +5,16 @@ import java.util.HashMap;
 public class warmUpAfterLongBreakLOL {
   public static void main(String[] args) {
     System.out.println("Heyyyyyyyyyyyyyyyyo X");
+    System.out.println("hey : | ");
     HashMap<Integer, String> test1 = new HashMap<>();
     test1.put(1, "ONE");
     test1.put(2, "two");
     test1.forEach((i, s) -> {
       System.out.println(i + "-> " + s);
     });
+    System.out.println("___________");
+    Runtime.getRuntime();
+    System.out.println(Runtime.version());
 
   }
 
