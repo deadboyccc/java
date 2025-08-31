2db980adbc5b45016e77a8ff2ec3760138829f23
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-08-31 17:58:57 +0300

    Implement ChannelSelectorServer for non-blocking I/O and multiple client handling

---
+++ javamc/src/main/java/com/advanced/ChannelSelectorServer.java
@@ -0,0 +1,50 @@
+package com.advanced;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ChannelSelectorServer {
+  // event driven
+  public static void main(String[] args) {
+    try (ServerSocketChannel serverChannel = ServerSocketChannel.open()) {
+
+      // server
+      serverChannel.bind(new InetSocketAddress(5000));
+      serverChannel.configureBlocking(false);
+
+      // selector
+      Selector selector = Selector.open();
+
+      // registering
+      serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+      // Main Loop -> constantly polling for events
+      while (true) {
+        selector.select();
+        Set<SelectionKey> selectedKey = selector.selectedKeys();
+        Iterator<SelectionKey> iterator = selectedKey.iterator();
+        while (iterator.hasNext()) {
+          SelectionKey key = iterator.next();
+          iterator.remove();
+          // ops
+          if (key.isAcceptable()) {
+            SocketChannel clientChannel = serverChannel.accept();
+          }
+
+        }
+
+      }
+
+    } catch (IOException io) {
+      System.out.println(io.getLocalizedMessage());
+    }
+
+  }
+
+}
