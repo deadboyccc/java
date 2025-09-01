eebe4016e68d1837eac019dcb271024b2a02a1ad
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-09-01 14:29:22 +0300

    Add URIURLDemos class for fetching and displaying JSON data from a URL

---
+++ javamc/src/main/java/com/advanced/URIURLDemos.java
@@ -0,0 +1,25 @@
+package com.advanced;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URIURLDemos {
+  public static void main(String[] args) {
+    try {
+      URL jsonPlaceholder = new URL("https://jsonplaceholder.typicode.com/todos/1");
+      var stream = jsonPlaceholder.openConnection().getInputStream();
+      InputStreamReader isr = new InputStreamReader(stream);
+      int data;
+      while ((data = isr.read()) != -1) {
+        System.out.print((char) data);
+      }
+      isr.close();
+      stream.close();
+    } catch (IOException e) {
+      System.out.println(e.getLocalizedMessage());
+      e.printStackTrace();
+    }
+  }
+}
